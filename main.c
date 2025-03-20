f947dafca5de3af7bb06f5988a891ef9b2743348
Okamito2006 <calidung2508@gmail.com>
2025-03-20 16:58:01 +0700

    update

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
     Yêu cầu 1: Viết hàm để kiểm tra xem tên khách hàng có chứa ký tự số không. Nếu có, yêu cầu khách hàng nhập lại tên.
     */
    //Khai báo biến và kiểu dữ liệu để lưu tên khách hàng
-    char name[100]= "aabbc";
+    char name[100]= "conmeo";
     printf("Check: %d\n", checkDigitInStr(name));
     do
     {
